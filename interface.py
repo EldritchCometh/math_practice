fecb6e73ca2f23f05423a7e1be2bf1c22eaae76a
admin01 <abanzhof@gmail.com>
2023-06-22 06:20:25 -0400

    .

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class FlashCardsWindow(tk.Tk):
@@ -6,7 +7,7 @@ class FlashCardsWindow(tk.Tk):
     def __init__(self, window_width, window_height):
         super().__init__()
         self.window(window_width, window_height)
-        self.current_frame = FrameOne(self)
+        self.current_frame = Problem(self)
         self.current_frame.pack(fill="both", expand=True)
 
     def window(self, window_width, window_height):
@@ -16,42 +17,74 @@ class FlashCardsWindow(tk.Tk):
         self.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
     def clicked(self, flag):
-        self.current_frame.destroy()
-        if flag == 'frame_1':
-            self.current_frame = FrameTwo(self)
-        if flag == 'frame_2':
-            self.current_frame = FrameOne(self)
-        self.current_frame.pack(fill="both", expand=True)
+        pass
 
 
-class FrameOne(tk.Frame):
+class Problem(tk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.build_layout()
+        self.make_grid()
+        self.question = self.make_question()
+        self.entry = self.make_entry()
+        self.make_timer_bar()
+        self.make_progress_bar()
+        self.last_window_size = (parent.winfo_width(), parent.winfo_height())
+        self.bind("<Configure>", self.resize_fonts)
 
-    def build_layout(self):
-        tk.Button(self, text="Foo", command=self.clicked).pack()
+    def check_answer(self, _=None):
+        print(self.entry.get())
 
-    def clicked(self):
-        self.parent.clicked("frame_1")
+    def resize_fonts(self, _=None):
+        window_size = (self.parent.winfo_width(), self.parent.winfo_height())
+        if not window_size == self.last_window_size:
+            self.last_window_size = window_size
+            font_size = min(
+                int(window_size[0] * 0.12),
+                int(window_size[1] * 0.7))
+            self.question.config(font=("Arial", font_size))
+            self.entry.config(font=("Arial", font_size))
 
+    def make_question(self):
+        question_frame = tk.Frame(self)
+        question_frame.grid(row=0, column=0, sticky="nsew")
+        label = ttk.Label(question_frame, text="22 + 22 =", font=("Arial", 108))
+        label.place(relx=0.5, rely=0.5, anchor='center')
+        return label
 
-class FrameTwo(tk.Frame):
+    def make_entry(self):
+        entry_frame = tk.Frame(self)
+        entry_frame.grid(row=0, column=1, sticky="nsew")
+        entry = ttk.Entry(
+            entry_frame, justify="center", width=2, font=("Arial", 108))
+        entry.pack(fill="both", expand=True)
+        entry.bind("<Return>", self.check_answer)
+        entry.bind("<KP_Enter>", self.check_answer)
+        entry.focus_set()
+        return entry
 
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
-        self.build_layout()
+    def make_timer_bar(self):
+        timer_frame = tk.Frame(self)
+        timer_frame.grid(row=1, column=0, columnspan=2, sticky="nsew")
+        timer = ttk.Progressbar(timer_frame, maximum=100, value=100)
+        timer.pack(fill="both", expand=True)
 
-    def build_layout(self):
-        tk.Button(self, text="Bar", command=self.clicked).pack()
+    def make_progress_bar(self):
+        prog_frame = tk.Frame(self)
+        prog_frame.grid(row=2, column=0, columnspan=2, sticky="nsew")
+        prog = ttk.Progressbar(prog_frame, maximum=100, value=100)
+        prog.pack(fill="both", expand=True)
 
-    def clicked(self):
-        self.parent.clicked("frame_2")
+    def make_grid(self):
+        self.grid_rowconfigure(0, weight=5)
+        self.grid_rowconfigure(1, weight=1, minsize=20)
+        self.grid_rowconfigure(2, weight=1, minsize=20)
+        self.grid_columnconfigure(0, weight=5)
+        self.grid_columnconfigure(1, weight=1)
 
 
 if __name__ == "__main__":
-    window = FlashCardsWindow(500, 300)
+
+    window = FlashCardsWindow(1100, 200)
     window.mainloop()
